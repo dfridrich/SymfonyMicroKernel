4be25494c6f631c0457df0e814c0343ef1fd4915
Dennis Fridrich <fridrich.dennis@gmail.com>
2015-12-17 10:30:18 +0100

    Initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/hello/{name}", name="hello")
+     * @Template()
+     */
+    public function helloAction($name)
+    {
+        return ['name' => $name];
+    }
+}
